8ae5abd4ab26f5cc906fda5d3ccfcda02b690a13
victormodino <victor.modgar@educa.jcyl.es>
2021-10-18 13:08:57 +0000

    ds

--- cookies_3.php
+++ cookies_3.php
@@ -1,20 +1,25 @@
 <?php 
 
 include 'head.php';
+
+
+
 if (isset($_REQUEST['entrar']))//boton entrar
     {
         if (isset($_request['guardar_password']))
         {//definimos usuario y contraseña
-            $usuario=$_request['usuario'];
-            $password=$_request['password'];
             //a cada usuario y contraseña le damos un tiempo de duracion para guardar la cookie
-            setcookie("usuario",$_POST,time()+2424);
-            setcookie("password".$$_POST,time()+2424);
+            setcookie("usuario",$_REQUEST['usuario'],time()+2424);
+            setcookie("password".$_REQUEST['password'],time()+2424);
 // ahora ponemos que nos imprima las cookies de usuario y password
             echo $_COOKIE['usuario'], "<br>", $_COOKIE['password'];
 
         }
 
+    }
+    if(isset($_COOKIE['usuario']))
+    {
+        $usuario=$_COOKIE['usuario'];
     }
 
 
@@ -24,10 +29,10 @@ echo'<form action="cookies_3.php" method="post">
 <table>
 <tr>
 <td>Usuario:</td>
-<td><input type="text" name="usuario" required></td>
+<td><input type="text" name="usuario"  required></td>
 </tr><tr>
 <td>Clave:</td>
-<td><input type="password" name="clave" required></td>
+<td><input type="password" name="clave"  required></td>
 </tr><tr>
 <td colspan="2"><input type="checkbox" name="guardar_clave" value="1"> Memorizar el usuario en este ordenador</td>
 </tr><tr>
