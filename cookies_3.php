a56de59d8203a82eef125c6c4b2f3ba4bafae434
victormodino <victor.modgar@educa.jcyl.es>
2021-10-18 15:01:03 +0200

    corregido el 3

--- cookies_3.php
+++ cookies_3.php
@@ -2,37 +2,38 @@
 
 include 'head.php';
 
+$usuario="";
+$password="";
 
 
 if (isset($_REQUEST['entrar']))//boton entrar
     {
-        if (isset($_request['guardar_password']))
+        if($_request['guardar_password'])//si pulsas el check sigue por aquí
         {//definimos usuario y contraseña
             //a cada usuario y contraseña le damos un tiempo de duracion para guardar la cookie
             setcookie("usuario",$_REQUEST['usuario'],time()+2424);
-            setcookie("password".$_REQUEST['password'],time()+2424);
-// ahora ponemos que nos imprima las cookies de usuario y password
-            echo $_COOKIE['usuario'], "<br>", $_COOKIE['password'];
-
+            setcookie("password",$_REQUEST['password'],time()+2424);
         }
 
-    }
-    if(isset($_COOKIE['usuario']))
+    }//fin de entrar
+    // ahora ponemos que nos guarde las cookies de usuario y password
+if(isset($_COOKIE['usuario']))
     {
         $usuario=$_COOKIE['usuario'];
+        $password=$_COOKIE['password'];
     }
 
 
-
+//ahora en el input usuario y contraseña tenemos que meter la variable en el value
 echo'<h1>Autenticar usuario y guardar en una cookie con PHP</h1><br><br>';
 echo'<form action="cookies_3.php" method="post">
 <table>
 <tr>
 <td>Usuario:</td>
-<td><input type="text" name="usuario"  required></td>
+<td><input type="text" name="usuario" value="'.$usuario.'" required></td>
 </tr><tr>
 <td>Clave:</td>
-<td><input type="password" name="clave"  required></td>
+<td><input type="password" name="clave" value="'.$password.'" required></td>
 </tr><tr>
 <td colspan="2"><input type="checkbox" name="guardar_clave" value="1"> Memorizar el usuario en este ordenador</td>
 </tr><tr>
