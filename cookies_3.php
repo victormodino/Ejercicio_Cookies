c5c0c043a434fcf4732f54dc18a6667ab8821065
victormodino <victor.modgar@educa.jcyl.es>
2021-10-18 20:55:15 +0200

    a

--- cookies_3.php
+++ cookies_3.php
@@ -1,27 +1,26 @@
-<?php 
+<?php
 
 include 'head.php';
-
+    
 $usuario="";
-$pass="";
-
+$password="";
 
 if(isset($_REQUEST['entrar']))//boton entrar
-    {
-        if($_request['guardar_clave'])//si pulsas el check sigue por aquí
-        {//definimos usuario y contraseña
-            //a cada usuario y contraseña le damos un tiempo de duracion para guardar la cookie
-            setcookie("user",$_REQUEST['usuario'],time()+2424);
-            setcookie("clave",$_REQUEST['clave'],time()+2424);
-        }
-
-    }//fin de entrar
-    // ahora ponemos que nos guarde las cookies de usuario y password
-if(isset($_COOKIE['user']))
-    {
-        $usuario=$_COOKIE['user'];
-        $pass=$_COOKIE['clave'];
+{
+    if(isset($_REQUEST['guardar_password']))//si pulsas el check sigue por aquí
+    {//a cada usuario y contraseña le damos un tiempo de duracion para guardar la cookie
+        setcookie('user',$_REQUEST['usuario'], time()+ 3600);
+        setcookie('pass',$_REQUEST['clave'], time()+ 3600);
     }
+
+}//fin de entrar   
+    // ahora ponemos que nos guarde las cookies de usuario y password
+    
+if(isset($_COOKIE['user']))
+{
+    $user=$_COOKIE['user'];
+    $pass=$_COOKIE['pass'];
+}
 
 
 //ahora en el input usuario y contraseña tenemos que meter la variable en el value
@@ -32,17 +31,16 @@ echo'<form action="cookies_3.php" method="post">
 <td>Usuario:</td>
 <td><input type="text" name="usuario" value="'.$usuario.'" required></td>
 </tr><tr>
-<td>Clave:</td>
-<td><input type="password" name="clave" value="'.$pass.'" required></td>
+<td>Contraseña:</td>
+<td><input type="password" name="clave" value="'.$password.'" required></td>
 </tr><tr>
-<td colspan="2"><input type="checkbox" name="guardar_clave" value="1"> Memorizar el usuario en este ordenador</td>
+<td colspan="2"><input type="checkbox" name="guardar_password" value="1" checked="checked"> Memorizar el usuario en este ordenador</td>
 </tr><tr>
 
-<td><input type="submit" value="Entrar"></td>
+<td><input type="submit" name="entrar" value="Entrar"></td>
 </tr>
 </table>
 </form> ';
-
 
 
 include 'pie.php';
